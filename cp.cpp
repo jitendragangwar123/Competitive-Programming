9633bc4831175d37b44685022ea590ffec1a8dc4
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2020-12-30 10:53:46 +0530

    Add files via upload

---
+++ cp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+    for(int i=0;i<10;i++){
+        cout<<"print the cout"<<'\n';
+        printf("print the printf\n");
+    }
+    return 0;
+}
