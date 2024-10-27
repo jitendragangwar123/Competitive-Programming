a2c718e80d6ea02773c536be0991be09f7f95d64
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-10-27 23:27:49 +0530

    feat:Minimum Changes To Make Alternating Binary String

--- Leetcode-Problems/Create Minimum_Changes_To_Make_Alternating_Binary_String.py
+++ Leetcode-Problems/Create Minimum_Changes_To_Make_Alternating_Binary_String.py
@@ -1,3 +1,4 @@
+# First Method
 class Solution:
     def minOperations(self, s: str) -> int:
         res=0
@@ -13,4 +14,19 @@ class Solution:
                 else:
                     0         
 
-        return min(res,len(s)-res)            
+        return min(res,len(s)-res) 
+
+# Second Method
+class Solution:
+    def minOperations(self, s: str) -> int:
+        res1 = 0  
+        res2 = 0  
+
+        for i, c in enumerate(s):
+            if (i % 2 == 0 and c == '1') or (i % 2 == 1 and c == '0'):
+                res1 += 1
+
+            if (i % 2 == 0 and c == '0') or (i % 2 == 1 and c == '1'):
+                res2 += 1
+
+        return min(res1, res2)
